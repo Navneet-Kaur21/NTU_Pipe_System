3164bb512decd11b0b13fc580fcfce043350a72c
navneet0401 <navneet.kaur@iitgn.ac.in>
2021-07-06 14:28:42 +0530

    Updated file for Collinearity check

--- Scripts/Collinear.py
+++ Scripts/Collinear.py
@@ -13,7 +13,6 @@ line_list = extracting.extractingLinePoints(centerlinesFileName)
 
 line_data = []
 line_data.append(line_list[1:])
-
 x0, y0, z0 = extracting.individualComponents(5, 6, 7, line_data)
 x1, y1, z1 = extracting.individualComponents(8, 9, 10, line_data)
 
@@ -62,12 +61,63 @@ file.writelines(lines_final)
 file.close()
 maxi = 0
 
+x0_f=[]
+x1_f=[]
+y0_f=[]
+y1_f=[]
+z0_f=[]
+z1_f=[]
+r_f=[]
+M_f=[]
+
 for i in range(length_data):
 
     for j in range(length_data):
         if i!=j:
             l = math.sqrt((p0[j][0]-p1[i][0])**2 + (p0[j][1]-p1[i][1])**2 + (p0[j][2]-p1[i][2])**2)
-            if l<2:
+            if l<1 and abs(M[i][0]-M[j][0])<0.001 and abs(M[i][1]-M[j][1])<0.001 and abs(M[i][2]-M[j][2])<0.001 and abs(rad[i]-rad[j])<0.001:
                 maxi = maxi+1
+                x0_f.append(x0[i])
+                x1_f.append(x1[j])
+                y0_f.append(y0[i])
+                y1_f.append(y1[j])
+                z0_f.append(z0[i])
+                z1_f.append(z1[j])
+                M_f.append(M[j])
+                r_f.append(rad[j])
 
-print(maxi)
+lines_f = []
+
+for i in range(len(x0_f)):
+    
+    lines_f.append(str(r_f[i]) + " " + str(x0_f[i]) + " " + str(y0_f[i]) + " " + str(z0_f[i]) + " " + 
+                    str(x1_f[i]) + " " + str(y1_f[i]) + " " + str(z1_f[i]) + " " + str(M_f[i][0]) + " " + 
+                    str(M_f[i][1]) + " " + str(M_f[i][2]) + "\n" )
+
+file_f = open('DataExtended.txt', 'w')
+
+file_f.truncate(0)
+
+file_f.writelines(lines_f)
+
+file_f.close()
+
+fig = plt.figure(1)
+ax = fig.gca(projection='3d')
+
+for i in range(length_data):
+    x = [x0[i], x1[i]]
+    y = [y0[i], y1[i]]
+    z = [z0[i], z1[i]]
+
+    figure = ax.plot(x, y, z)
+
+
+for i in range(len(x0_f)):
+    x = [x0_f[i], x1_f[i]]
+    y = [y0_f[i], y1_f[i]]
+    z = [z0_f[i], z1_f[i]]
+
+    figure = ax.plot(x, y, z, 'black')
+
+plt.show()
